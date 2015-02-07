e8011932355102d9b82117c41e22911654d025e4
neothemachine <maik.riechert@arcor.de>
2015-02-07 15:43:38 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'crowfood',
+    version = '0.1.0',
+    author = 'Maik Riechert',
+    author_email = 'maik.riechert@arcor.de',
+    url = 'https://github.com/neothemachine/crowfood',
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Natural Language :: English',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Operating System :: OS Independent',
+    ],
+    packages = find_packages(),
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'cfood = crowfood.crowfood:main',
+        ]
+    }
+)
