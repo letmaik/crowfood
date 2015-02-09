3d468cb31cd2203e01e0197f0d6bbec6a0bb726d
neothemachine <maik.riechert@arcor.de>
2015-02-09 21:04:54 +0100

    allow to read regex clusters from file

--- crowfood/cluster_regexp.py
+++ crowfood/cluster_regexp.py
@@ -17,11 +17,29 @@ def iterpairs(thelist):
     while 1:
         yield next(ilist), next(ilist)
 
+def read_clusters(fn):
+    "Return a list of cluster regexes read from the file 'fn'."
+    f = open(fn, 'rU')
+    clusters = []
+    for x in imap(str.strip, f.xreadlines()):
+        if not x:
+            continue
+        regex, rename = x.split(' ')
+        clusters.append(regex)
+        clusters.append(rename)
+    return clusters
 
 def main():
     import optparse
     parser = optparse.OptionParser(__doc__.strip())
+    
+    parser.add_option('-f', '--from-file', action='store',
+                      help="Read cluster list from the given filename.")
+    
     opts, renames = parser.parse_args()
+
+    if opts.from_file:
+        renames.extend(read_clusters(opts.from_file))
 
     if len(renames) % 2:
         parser.error("An odd number of renames was specified.")
