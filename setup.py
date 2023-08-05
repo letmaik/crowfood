25586ae800465d5e22baf7b8d5d61be305d1e156
Maik Riechert <letmaik@outlook.com>
2023-08-05 21:59:18 +0100

    remove optional author_email field

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     long_description = open('README.rst').read(),
     version = '0.4.1',
     author = 'Maik Riechert',
-    author_email = 'maik.riechert@arcor.de',
-    url = 'https://github.com/neothemachine/crowfood',
+    url = 'https://github.com/letmaik/crowfood',
     classifiers=[
       'Development Status :: 4 - Beta',
       'Natural Language :: English',
